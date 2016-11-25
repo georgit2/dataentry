f5b0f0865eadc45473a2ae5589c4144b3b837904
Iam_moore <georgit2@users.noreply.github.com>
2016-11-25 12:03:44 +0000

    Add files via upload

--- Election Results/src/DataEntry.java
+++ Election Results/src/DataEntry.java
@@ -10,6 +10,7 @@ import java.awt.Color;
 import javax.swing.SwingConstants;
 import javax.swing.JSplitPane;
 import javax.swing.JButton;
+import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
@@ -20,11 +21,14 @@ import java.util.Properties;
 import java.sql.PreparedStatement;
 import javax.swing.JPanel;
 import java.io.*;
-
+import java.util.*;
+import java.awt.image.BufferedImage;
+import java.awt.SystemColor;
+import javax.swing.UIManager;
 
 public class DataEntry {
 
-	private JFrame frame;
+	private JFrame frmResultsDataEntry;
 	private JTextField pol_code;
 	private JTextField cppinput;
 	private JTextField ndpinput;
@@ -45,204 +49,187 @@ public class DataEntry {
 			public void run() {
 				try {
 					DataEntry window = new DataEntry();
-					window.frame.setVisible(true);
+					window.frmResultsDataEntry.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
 			}
 		});
-
-
-
-
-
 	}
 
 	/**
 	 * Create the application.
 	 */
+	
 	public DataEntry() {
 		initialize();
-		showImage(pos);
 	}
 	
-	int pos = 0;
-	JLabel lblImage = new JLabel("C:\\Users\\JARVIS\\Pictures\\images");
-	public void showImage(int index)
-	{
-		String[] imageList = getImages();
-		String imageName = imageList[index];
-		ImageIcon icon = new ImageIcon("C:\\Users\\JARVIS\\Pictures\\images"+imageName);
-		Image image = icon.getImage().getScaledInstance(lblImage.getWidth(), lblImage.getHeight(), Image.SCALE_SMOOTH);
-		lblImage.setIcon(new ImageIcon(image));
-		
-	}
-	
-	public String[] getImages(){
-		File file = new File("");
-		String[] imageList = file.list();
-		return imageList;
-	}
-	
+	 
+	int index = 0;
 	
 	/**
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
-		frame = new JFrame();
-		frame.getContentPane().setBackground(new Color(175, 238, 238));
-		frame.setBounds(100, 100, 1100, 648);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.getContentPane().setLayout(null);
+		frmResultsDataEntry = new JFrame();
+		frmResultsDataEntry.setTitle("Results Data Entry Center");
+		frmResultsDataEntry.getContentPane().setBackground(UIManager.getColor("InternalFrame.activeTitleGradient"));
+		frmResultsDataEntry.setBounds(100, 100, 1100, 648);
+		frmResultsDataEntry.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frmResultsDataEntry.getContentPane().setLayout(null);
 		
 		JLabel lblCode = new JLabel("CODE:");
 		lblCode.setFont(new Font("Arial", Font.PLAIN, 14));
 		lblCode.setBounds(28, 37, 56, 16);
-		frame.getContentPane().add(lblCode);
+		frmResultsDataEntry.getContentPane().add(lblCode);
 		
 		pol_code = new JTextField();
 		pol_code.setBackground(new Color(245, 245, 245));
 		pol_code.setBounds(120, 34, 116, 22);
-		frame.getContentPane().add(pol_code);
+		frmResultsDataEntry.getContentPane().add(pol_code);
 		pol_code.setColumns(10);
 		
 		JLabel lblCpp = new JLabel("CPP");
 		lblCpp.setEnabled(true);
 		lblCpp.setFont(new Font("Arial", Font.PLAIN, 13));
 		lblCpp.setBounds(28, 89, 56, 16);
-		frame.getContentPane().add(lblCpp);
+		frmResultsDataEntry.getContentPane().add(lblCpp);
 		
 		cppinput = new JTextField();
 		cppinput.setFont(new Font("Tahoma", Font.PLAIN, 15));
 		cppinput.setBounds(120, 86, 116, 22);
-		frame.getContentPane().add(cppinput);
+		frmResultsDataEntry.getContentPane().add(cppinput);
 		cppinput.setColumns(10);
 		
 		JLabel lblNdp = new JLabel("NDP");
 		lblNdp.setFont(new Font("Arial", Font.PLAIN, 13));
 		lblNdp.setBounds(28, 138, 56, 16);
-		frame.getContentPane().add(lblNdp);
+		frmResultsDataEntry.getContentPane().add(lblNdp);
 		
 		ndpinput = new JTextField();
 		ndpinput.setFont(new Font("Tahoma", Font.PLAIN, 15));
 		ndpinput.setBounds(120, 135, 116, 22);
-		frame.getContentPane().add(ndpinput);
+		frmResultsDataEntry.getContentPane().add(ndpinput);
 		ndpinput.setColumns(10);
 		
 		JLabel lblNdc = new JLabel("NDC");
 		lblNdc.setFont(new Font("Arial", Font.PLAIN, 13));
 		lblNdc.setBounds(28, 188, 56, 16);
-		frame.getContentPane().add(lblNdc);
+		frmResultsDataEntry.getContentPane().add(lblNdc);
 		
 		ndcinput = new JTextField();
 		ndcinput.setFont(new Font("Tahoma", Font.PLAIN, 15));
 		ndcinput.setBounds(120, 185, 116, 22);
-		frame.getContentPane().add(ndcinput);
+		frmResultsDataEntry.getContentPane().add(ndcinput);
 		ndcinput.setColumns(10);
 		
 		JLabel lblPpp = new JLabel("PPP");
 		lblPpp.setFont(new Font("Arial", Font.PLAIN, 13));
 		lblPpp.setBounds(28, 240, 56, 16);
-		frame.getContentPane().add(lblPpp);
+		frmResultsDataEntry.getContentPane().add(lblPpp);
 		
 		pppinput = new JTextField();
 		pppinput.setFont(new Font("Tahoma", Font.PLAIN, 15));
 		pppinput.setBounds(120, 237, 116, 22);
-		frame.getContentPane().add(pppinput);
+		frmResultsDataEntry.getContentPane().add(pppinput);
 		pppinput.setColumns(10);
 		
 		JLabel lblNpp = new JLabel("NPP");
 		lblNpp.setFont(new Font("Arial", Font.PLAIN, 13));
 		lblNpp.setBounds(28, 294, 56, 16);
-		frame.getContentPane().add(lblNpp);
+		frmResultsDataEntry.getContentPane().add(lblNpp);
 		
 		nppinput = new JTextField();
 		nppinput.setFont(new Font("Tahoma", Font.PLAIN, 15));
 		nppinput.setBounds(120, 291, 116, 22);
-		frame.getContentPane().add(nppinput);
+		frmResultsDataEntry.getContentPane().add(nppinput);
 		nppinput.setColumns(10);
 		
 		JLabel lblPnc = new JLabel("PNC");
 		lblPnc.setFont(new Font("Arial", Font.PLAIN, 13));
 		lblPnc.setBounds(28, 355, 56, 16);
-		frame.getContentPane().add(lblPnc);
+		frmResultsDataEntry.getContentPane().add(lblPnc);
 		
 		pncinput = new JTextField();
 		pncinput.setFont(new Font("Tahoma", Font.PLAIN, 15));
 		pncinput.setBounds(120, 352, 116, 22);
-		frame.getContentPane().add(pncinput);
+		frmResultsDataEntry.getContentPane().add(pncinput);
 		pncinput.setColumns(10);
 		
 		JLabel lblInd = new JLabel("IND");
 		lblInd.setFont(new Font("Arial", Font.PLAIN, 13));
 		lblInd.setBounds(28, 410, 56, 16);
-		frame.getContentPane().add(lblInd);
+		frmResultsDataEntry.getContentPane().add(lblInd);
 		
 		indinput = new JTextField();
 		indinput.setFont(new Font("Tahoma", Font.PLAIN, 15));
 		indinput.setBounds(120, 407, 116, 22);
-		frame.getContentPane().add(indinput);
+		frmResultsDataEntry.getContentPane().add(indinput);
 		indinput.setColumns(10);
 		
 		JLabel lblRejectedVotes = new JLabel("Rejected Votes");
 		lblRejectedVotes.setFont(new Font("Arial", Font.PLAIN, 13));
 		lblRejectedVotes.setBounds(12, 461, 94, 28);
-		frame.getContentPane().add(lblRejectedVotes);
+		frmResultsDataEntry.getContentPane().add(lblRejectedVotes);
 		
 		rejected_votes_input = new JTextField();
 		rejected_votes_input.setFont(new Font("Tahoma", Font.PLAIN, 15));
 		rejected_votes_input.setBounds(120, 464, 116, 22);
-		frame.getContentPane().add(rejected_votes_input);
+		frmResultsDataEntry.getContentPane().add(rejected_votes_input);
 		rejected_votes_input.setColumns(10);
 		
 		JLabel lblValidVotes = new JLabel("Valid Votes");
 		lblValidVotes.setFont(new Font("Arial", Font.BOLD, 13));
 		lblValidVotes.setBounds(332, 37, 83, 33);
-		frame.getContentPane().add(lblValidVotes);
+		frmResultsDataEntry.getContentPane().add(lblValidVotes);
 		
 		JLabel lblV_votes = new JLabel("");
 		lblV_votes.setFont(new Font("Arial", Font.BOLD, 14));
 		lblV_votes.setHorizontalAlignment(SwingConstants.CENTER);
 		lblV_votes.setBounds(455, 37, 114, 33);
-		frame.getContentPane().add(lblV_votes);
+		frmResultsDataEntry.getContentPane().add(lblV_votes);
 		
 		JLabel lblTotalVotes = new JLabel("Total Votes");
 		lblTotalVotes.setFont(new Font("Arial", Font.BOLD, 13));
 		lblTotalVotes.setBounds(332, 118, 83, 42);
-		frame.getContentPane().add(lblTotalVotes);
+		frmResultsDataEntry.getContentPane().add(lblTotalVotes);
 		
 		JLabel lblT_votes = new JLabel("");
 		lblT_votes.setBackground(new Color(255, 255, 255));
 		lblT_votes.setFont(new Font("Arial", Font.BOLD, 14));
 		lblT_votes.setHorizontalAlignment(SwingConstants.CENTER);
 		lblT_votes.setBounds(455, 118, 114, 36);
-		frame.getContentPane().add(lblT_votes);
+		frmResultsDataEntry.getContentPane().add(lblT_votes);
+		
+		JLabel lblImage = new JLabel("");
+		lblImage.setBackground(new Color(230, 230, 250));
+		lblImage.setForeground(new Color(220, 220, 220));
+		lblImage.setBounds(581, 13, 474, 493);
+		frmResultsDataEntry.getContentPane().add(lblImage);
+		
+		File path = new File("C:/Users/JARVIS/Documents/abbyy/images/");
+		File[] files = path.listFiles();
 		
 		JButton btnNewButton_1 = new JButton("Fetch Image");
 		btnNewButton_1.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
-				pos = pos + 1;
-			       if(pos >= getImages().length)
-			       {
-			           pos  = getImages().length - 1;
-			       }
-			       showImage(pos);
-			    }   
+		        for(index = 0; index < files.length; index++) {
+		        ImageIcon[] imagelist = new ImageIcon[files.length];
+				//Image image = imagelist[index].getImage().getScaledInstance(lblImage.getWidth(), lblImage.getHeight(), Image.SCALE_SMOOTH);
+				 lblImage.setIcon(imagelist[index]);
+		        }
+			}
 		});
-		btnNewButton_1.setBounds(767, 538, 106, 50);
-		frame.getContentPane().add(btnNewButton_1);
-		
-		
-		lblImage.setBackground(new Color(230, 230, 250));
-		lblImage.setForeground(new Color(220, 220, 220));
-		lblImage.setBounds(581, 13, 474, 512);
-		frame.getContentPane().add(lblImage);
-		
+		btnNewButton_1.setBounds(758, 519, 116, 42);
+		frmResultsDataEntry.getContentPane().add(btnNewButton_1);
 		
 		
 		JButton btnNewButton = new JButton("Submit");
 		btnNewButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
+				
+				String Pol_code = pol_code.getText();
 				
 				cpp = Integer.parseInt(cppinput.getText());
 				ndp = Integer.parseInt(ndpinput.getText());
@@ -256,8 +243,7 @@ public class DataEntry {
 				validvotes = cpp + ndp + ndc + ppp + npp + pnc + ind;
 				totalvotes = rejectedvotes + validvotes;
 				
-				String Pol_code = pol_code.getText();
-				
+								
 				//Database connection intialization
 				Connection connect = null;
 				
@@ -281,9 +267,9 @@ public class DataEntry {
 					prepstatement.setInt(5, ndp);
 					prepstatement.setInt(6, pnc);
 					prepstatement.setInt(7, ind);
-					prepstatement.setInt(8, totalvotes);
-					prepstatement.setInt(9, rejectedvotes);
-					prepstatement.setInt(10, validvotes);
+					prepstatement.setInt(8, rejectedvotes);
+					prepstatement.setInt(9, validvotes);
+					prepstatement.setInt(10, totalvotes);
 					
 					prepstatement.setString(11, Pol_code);
 
@@ -293,7 +279,7 @@ public class DataEntry {
 					String t_votes = Integer.toString(totalvotes);
 					lblV_votes.setText(v_votes);
 					lblT_votes.setText(t_votes);
-					
+					System.out.println("Database updated");
 					connect.close();
 				}catch (Exception e) {
 					System.err.println("Got an exception");
@@ -304,8 +290,8 @@ public class DataEntry {
 		});
 		btnNewButton.setHorizontalAlignment(SwingConstants.LEFT);
 		btnNewButton.setIcon(new ImageIcon("C:\\Users\\JARVIS\\Documents\\abbyy\\sign-check-icon.png"));
-		btnNewButton.setBounds(299, 507, 116, 42);
-		frame.getContentPane().add(btnNewButton);
+		btnNewButton.setBounds(299, 519, 116, 42);
+		frmResultsDataEntry.getContentPane().add(btnNewButton);
 		
 	}
 }
